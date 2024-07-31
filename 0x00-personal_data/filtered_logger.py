646f2c96217e269bae9600cb19d363b1166f6ff0
loxiasblack <iliaschokairi@gmail.com>
2024-07-31 20:59:55 +0100

    push

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python3
+""" first interaction with pii"""
 from typing import List
 import re
 
-def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
-    return re.sub(rf'({"|".join(map(re.escape, fields))})=[^{separator}]*', lambda m: f"{m.group(1)}={redaction}", message)
 
+def filter_datum(fields: List[str],
+                 redaction: str, message: str,
+                 separator: str) -> str:
+    """ function that filter the list """
+    return re.sub(rf'({"|".join(map(re.escape, fields))})=[^{separator}]*',
+                  lambda m: f"{m.group(1)}={redaction}", message)
