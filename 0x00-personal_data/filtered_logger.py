eb4df83d29a1f58568b269f40d53ab8b50c3695c
loxiasblack <iliaschokairi@gmail.com>
2024-07-31 20:26:34 +0100

    try

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,12 +1,7 @@
 #!/usr/bin/env python3
-""" filtered logger """
 from typing import List
 import re
 
-
 def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
-    """ function that change pii by redaction"""
-    for field in fields:
-        pattern = re.compile(rf"{re.escape(field)}=[^\\{separator}]*")
-        new_message = pattern.sub(f"{field}={redaction}", message)
-    return new_message
+    return re.sub(rf'({"|".join(map(re.escape, fields))})=[^{separator}]*', lambda m: f"{m.group(1)}={redaction}", message)
+
