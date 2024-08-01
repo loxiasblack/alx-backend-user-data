e201113a79b513c84cc29a27eb1031f2f942c71a
loxiasblack <iliaschokairi@gmail.com>
2024-08-01 11:27:03 +0100

    return

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -2,6 +2,7 @@
 """ first interaction with pii"""
 from typing import List
 import re
+import logging
 
 
 def filter_datum(fields: List[str],
@@ -10,3 +11,20 @@ def filter_datum(fields: List[str],
     """ function that filter the list """
     return re.sub(rf'({"|".join(map(re.escape, fields))})=[^{separator}]*',
                   lambda m: f"{m.group(1)}={redaction}", message)
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        return filter_datum(self.fields, self.REDACTION,
+                            super().format(record), self.SEPARATOR)
---
+++ 0x00-personal_data/mylib.py
@@ -0,0 +1,5 @@
+import logging
+logger = logging.getLogger(__name__)
+
+def do_something():
+    logger.info('Doing something')
