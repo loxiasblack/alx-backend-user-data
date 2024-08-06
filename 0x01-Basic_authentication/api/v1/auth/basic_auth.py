e52df7023b7489c25bc8ce173391f68a48cfee4f
loxiasblack <iliaschokairi@gmail.com>
2024-08-06 11:13:11 +0100

    new method that exract user email and password

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -31,3 +31,17 @@ class BasicAuth(Auth):
         except Exception:
             return None
         return decode.decode('utf-8')
+
+    def extract_user_credentials(self,
+                                 decoded_base64_authorization_header:
+                                     str) -> (str, str):
+        """ extract user credentials method """
+        if decoded_base64_authorization_header is None:
+            return (None, None)
+        if not isinstance(decoded_base64_authorization_header, str):
+            return (None, None)
+        if ":" not in decoded_base64_authorization_header:
+            return (None, None)
+        user = decoded_base64_authorization_header.split(":")[0]
+        password = decoded_base64_authorization_header.split(":")[-1]
+        return (user, password)
