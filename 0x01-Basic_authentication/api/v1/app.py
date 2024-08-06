41fabe0342b2143260ef991e94abfba513da01ec
loxiasblack <iliaschokairi@gmail.com>
2024-08-06 10:31:01 +0100

    new basic_auth class

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -17,6 +17,9 @@ auth = None
 if getenv("AUTH_TYPE") == "auth":
     from api.v1.auth.auth import Auth
     auth = Auth()
+if getenv("AUTH_TYPE") == "basic_auth":
+    from api.v1.auth.basic_auth import BasicAuth
+    auth = BasicAuth()
 if auth:
     @app.before_request
     def before_request():
---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" new class that inherit from Auth class"""
+from api.v1.auth.auth import Auth
+
+
+class BasicAuth(Auth):
+    """ new basic class"""
+    pass
