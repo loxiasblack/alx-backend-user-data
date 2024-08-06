a568afe4245ab52d39dbe4e34eff92dd637be49e
loxiasblack <iliaschokairi@gmail.com>
2024-08-06 10:20:34 +0100

    new before_request handler

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -12,6 +12,26 @@ app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
+auth = None
+
+if getenv("AUTH_TYPE") == "auth":
+    from api.v1.auth.auth import Auth
+    auth = Auth()
+if auth:
+    @app.before_request
+    def before_request():
+        """ the before request method """
+        list_of_paths = ['/api/v1/status/',
+                         '/api/v1/unauthorized/',
+                         '/api/v1/forbidden/']
+        authoriztion = auth.require_auth(request.path, list_of_paths)
+        if not authoriztion:
+            return
+        if auth.authorization_header(request) is None:
+            abort(401)
+        if auth.current_user(request) is None:
+            abort(403)
+
 
 @app.errorhandler(404)
 def not_found(error) -> str:
--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -22,7 +22,11 @@ class Auth:
 
     def authorization_header(self, request=None) -> str:
         """ auth path method """
-        return None
+        if request is None:
+            return None
+        if "Authorization" not in request.headers.keys():
+            return None
+        return request.headers.get("Authorization")
 
     def current_user(self, request=None) -> TypeVar('User'):
         """ current_user method """
