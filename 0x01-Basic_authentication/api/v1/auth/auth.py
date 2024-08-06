7891ca4ede7d6455c6a2172b2ef5c1d20b69fe8e
loxiasblack <iliaschokairi@gmail.com>
2024-08-06 09:13:32 +0100

    new class

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -1,34 +1,21 @@
 #!/usr/bin/env python3
-""""""
+""" new class Auth """
 from typing import List, TypeVar
 from flask import request
 from api.v1.views.users import User
 
 
-
 class Auth:
-    """"""
-    
+    """ the class auth """
+
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """"""
-        if path is None:
-            return True
-        if excluded_paths is None or excluded_paths == []:
-            return True
-        if not path.endswith('/'):
-            path += '/'
-        if path in excluded_paths:
-            return False
-        return True
+        """ require path method """
+        return False
 
     def authorization_header(self, request=None) -> str:
-        """"""
-        if request is None:
-            return None
-        if not request.headers.get('Authorization'):
-            return None
-        return request.headers.get('Authorization')
+        """ auth path method """
+        return None
 
     def current_user(self, request=None) -> TypeVar('User'):
-        """"""
+        """ current_user method """
         return None
