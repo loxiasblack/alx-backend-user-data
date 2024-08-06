00896970b2bd11d6fa7ffdc40171323cf5d9ae94
loxiasblack <iliaschokairi@gmail.com>
2024-08-06 16:40:29 +0100

    new method in basic auth

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 """ new class that inherit from Auth class"""
+from typing import TypeVar
+from api.v1.views.users import User
 from api.v1.auth.auth import Auth
 from base64 import b64decode
 
@@ -45,3 +47,23 @@ class BasicAuth(Auth):
         user = decoded_base64_authorization_header.split(":")[0]
         password = decoded_base64_authorization_header.split(":")[-1]
         return (user, password)
+
+    def user_object_from_credentials(self,
+                                     user_email: str,
+                                     user_pwd: str) -> TypeVar('User'):
+        """ new method user object from credentials """
+        if not isinstance(user_email, str) or user_email is None:
+            return None
+
+        if not isinstance(user_pwd, str) or user_pwd is None:
+            return None
+
+        user_list = User.search({"email": user_email})
+        if not user_list:
+            return None
+
+        user = user_list[0]
+        if not user.is_valid_password(user_pwd):
+            return None
+
+        return user
