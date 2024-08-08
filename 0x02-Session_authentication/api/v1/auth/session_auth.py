ff0e2e36f39d213bab0f174212fff6a274690330
loxiasblack <iliaschokairi@gmail.com>
2024-08-08 08:57:31 +0100

    create my new class

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -6,22 +6,23 @@ from uuid import uuid4
 
 class SessionAuth(Auth):
     """ the class """
-    user_id_by_session_id = {}
+    # user_id_by_session_id = {}
 
-    def create_session(self, user_id: str = None) -> str:
-        """ create a session method """
-        if user_id is None:
-            return None
-        if not isinstance(user_id, str):
-            return None
-        session_id = str(uuid4())
-        self.user_id_by_session_id[session_id] = user_id
-        return session_id
+    # def create_session(self, user_id: str = None) -> str:
+    #     """ create a session method """
+    #     if user_id is None:
+    #         return None
+    #     if not isinstance(user_id, str):
+    #         return None
+    #     session_id = str(uuid4())
+    #     self.user_id_by_session_id[session_id] = user_id
+    #     return session_id
 
-    def user_id_for_session_id(self, session_id: str = None) -> str:
-        """ create user_id for session_id method """
-        if session_id is None:
-            return None
-        if not isinstance(session_id, str):
-            return None
-        return self.user_id_by_session_id.get(session_id)
+    # def user_id_for_session_id(self, session_id: str = None) -> str:
+    #     """ create user_id for session_id method """
+    #     if session_id is None:
+    #         return None
+    #     if not isinstance(session_id, str):
+    #         return None
+    #     return self.user_id_by_session_id.get(session_id)
+    pass
