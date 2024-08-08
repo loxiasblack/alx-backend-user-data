aa59bf105f1db32c2eb17d15698c4f8a1a5ab324
loxiasblack <iliaschokairi@gmail.com>
2024-08-08 22:51:20 +0100

    push

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -26,6 +26,9 @@ if getenv("AUTH_TYPE") == "session_auth":
 if getenv("AUTH_TYPE") == "session_exp_auth":
     from api.v1.auth.session_exp_auth import SessionExpAuth
     auth = SessionExpAuth()
+if getenv("AUTH_TYPE") == "session_db_auth":
+    from api.v1.auth.session_db_auth import SessionDBAuth
+    auth = SessionDBAuth()
 if auth:
     @app.before_request
     def before_request():
