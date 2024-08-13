3b10e2d2671c7a455c23328be317effc2f2bb652
loxiasblack <iliaschokairi@gmail.com>
2024-08-13 17:19:11 +0100

    app

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -80,7 +80,7 @@ def profile() -> str:
     user = AUTH.get_user_from_session_id(session_id)
     if not user:
         abort(403)
-    return jsonify({"email": user.email}), 200
+    return jsonify({"email": f"{user.email}"}), 200
 
 
 if __name__ == "__main__":
