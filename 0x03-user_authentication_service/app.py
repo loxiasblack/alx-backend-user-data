79766ae8976869e642113e45a325e483b2473679
loxiasblack <iliaschokairi@gmail.com>
2024-08-13 10:57:51 +0100

    new root

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3
 """ app flak """
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from auth import Auth
 
+
+AUTH = Auth()
 app = Flask(__name__)
 
 
@@ -10,6 +13,19 @@ app = Flask(__name__)
 def index():
     """ return the follow message """
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route("/users", methods=["POST"], strict_slashes=False)
+def users():
+    """ route to register a new user """
+    if request.method == "POST":
+        try:
+            AUTH.register_user(request.form["email"], request.form["password"])
+            return jsonify(
+                {"email": request.form["email"], "message": "user created"}
+                )
+        except ValueError:
+            return jsonify({"message": "email already registered"}), 400
 
 
 if __name__ == "__main__":
